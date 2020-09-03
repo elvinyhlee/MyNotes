e2de525b1a43a0a784ea83e5574c8d5e0b55e218
elvinyhlee <elvin.lee@bowtie.com.hk>
2020-09-03 21:06:22 +0800

    updates

--- .vuepress/config.js
+++ .vuepress/config.js
@@ -1,12 +1,28 @@
 module.exports = {
-  title: 'Hello Elvin',
+  title: 'Tech Notes',
+  description: 'Just playing around',
   themeConfig: {
     nav: [
-      { text: 'Another page', link: '/hello.html'}
+      { text: 'Data Structure', link: '/hello.html'},
+      { text: 'System Design', link: '/guide/' },
+      { text: 'My Website', link: 'https://elvinyhl.com/' }
     ],
     sidebar: [
       '/',
-      '/hello'
-    ]
+      '/hello',
+      {
+        title: 'Group 1',   // required
+        path: '/hello',      // optional, link of the title, which should be an absolute path and must exist
+        collapsable: false, // optional, defaults to true
+        sidebarDepth: 1,    // optional, defaults to 1
+        children: [
+          '/hello'
+        ]
+      },
+    ],
+    // lastUpdated: 'Last Updated',
+    logo: '/assets/img/hero.png',
+    repo: 'elvinyhlee/MyNotes',
+    repoLabel: 'Github',
   }
 };
--- README.md
+++ README.md
@@ -1,11 +1,15 @@
-# Hello VuePress
-
-Hello World
-
-> markdown
-
-::: tip
-hi
-:::
-
-[go to other page](hello.md)
+---
+home: true
+heroText: Tech Notes
+tagline: My personal notes on algorithm, data structure, system design and more.
+actionText: Get Started →
+actionLink: /guide/
+features:
+- title: Algorithm
+  details: Minimal setup with markdown-centered project structure helps you focus on writing.
+- title: System
+  details: Enjoy the dev experience of Vue + webpack, use Vue components in markdown, and develop custom themes with Vue.
+- title: Performant
+  details: VuePress generates pre-rendered static HTML for each page, and runs as an SPA once a page is loaded.
+footer: Elvin Lee © 2020 All rights reserved
+---
